fcb62ef601ed406f10336b9926eb0c72f466806d
leticia <le.augusta.souza@gmail.com>
2022-07-21 23:22:28 -0300

    Atualizações dos projetos 21 de jul 2022 às 23h

---
+++ Projeto-IntelliJ/Primeiro Projeto/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,12 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    Gato gato = new Gato();
+    public static void main(String[] args) {
+       /* int a = 5;
+        int b = 3;
+        System.out.println("Hello World!");*/
+    }
+}
